29f3a4c75e9913710d2a0c76b74bf7b6bcbdf96d
Santiago Aquino <santiagoaquino01@hotmail.com>
2023-05-30 11:54:14 -0300

    Ultimas modificaciones para el TP Integrador JS

--- js/index.js
+++ js/index.js
@@ -1,50 +1,78 @@
-const INPUT_CANTIDAD = document.getElementById("inputCantidad").value;
-const INPUT_CATEGORIA = document.getElementById("inputCategoria").value;
+const INPUT_CANTIDAD = document.getElementById("inputCantidad");
+const INPUT_CATEGORIA = document.getElementById("inputCategoria");
 const RESULTADO = document.getElementById("resultado");
 const BUTTON_RESUMEN = document.getElementById("buttonResumen");
+const INPUT_NOMBRE = document.getElementById("inputNombre");
+const INPUT_APELLIDO = document.getElementById("inputApellido");
+const INPUT_CORREO = document.getElementById("inputCorreo");
+const BUTTON_BORRAR = document.getElementById("buttonBorrar");
 
 BUTTON_RESUMEN.addEventListener("click", function (e) {
   e.preventDefault();
 
-  if (!INPUT_CANTIDAD) {
-    RESULTADO.innerHTML = "Ingrese una cantidad";
+  if (!validaciones()) {
+    return;
   }
 
-  if (INPUT_CATEGORIA === "Estudiante") {
-    let totalPago = INPUT_CANTIDAD * 200;
+  if (INPUT_CATEGORIA.value === "Estudiante") {
+    let totalPago = INPUT_CANTIDAD.value * 200;
     let descuento = totalPago * 0.8;
     RESULTADO.innerHTML = `Total a Pagar: $${totalPago - descuento}`;
-    informarPorConsola(INPUT_CANTIDAD, INPUT_CATEGORIA, totalPago - descuento);
   }
 
-  if (INPUT_CATEGORIA === "Trainee") {
-    let totalPago = INPUT_CANTIDAD * 200;
+  if (INPUT_CATEGORIA.value === "Trainee") {
+    let totalPago = INPUT_CANTIDAD.value * 200;
     let descuento = totalPago * 0.5;
     RESULTADO.innerHTML = `Total a Pagar: $${totalPago - descuento}`;
-    informarPorConsola(INPUT_CANTIDAD, INPUT_CATEGORIA, totalPago - descuento);
   }
 
-  if (INPUT_CATEGORIA === "Junior") {
-    let totalPago = INPUT_CANTIDAD * 200;
+  if (INPUT_CATEGORIA.value === "Junior") {
+    let totalPago = INPUT_CANTIDAD.value * 200;
     let descuento = totalPago * 0.15;
     RESULTADO.innerHTML = `Total a Pagar: $${totalPago - descuento}`;
-    informarPorConsola(INPUT_CANTIDAD, INPUT_CATEGORIA, totalPago - descuento);
   }
+  console.log({
+    nombre: INPUT_NOMBRE.value,
+    apellido: INPUT_APELLIDO.value,
+    correo: INPUT_CORREO.value,
+    cantidad: INPUT_CANTIDAD.value,
+    categoria: INPUT_CATEGORIA.value,
+    resultado: RESULTADO.innerHTML,
+  });
 });
 
-function informarPorConsola(cantidad, categoria, resultado) {
-  const INPUT_NOMBRE = document.getElementById("inputNombre").value;
-  const INPUT_APELLIDO = document.getElementById("inputApellido").value;
-  const INPUT_CORREO = document.getElementById("inputCorreo").value;
+function validaciones() {
+  if (
+    !INPUT_CANTIDAD.value ||
+    !INPUT_CORREO.value ||
+    !INPUT_NOMBRE.value ||
+    !INPUT_APELLIDO.value ||
+    !INPUT_CATEGORIA.value
+  ) {
+    RESULTADO.innerHTML = "Ingrese todos los datos";
+    return false;
+  }
 
-  const usuario = {
-    nombre: INPUT_NOMBRE,
-    apellido: INPUT_APELLIDO,
-    correo: INPUT_CORREO,
-    cantidad,
-    categoria,
-    resultado,
-  };
+  if (Number(INPUT_CANTIDAD.value <= 0)) {
+    RESULTADO.innerHTML =
+      "Tiene que ingresar un numero mayor a 0 en el campo cantidad";
+    return false;
+  }
 
-  console.log(usuario);
+  if (isNaN(parseInt(INPUT_CANTIDAD.value))) {
+    RESULTADO.innerHTML = "Tiene que ingresar un numero en el campo cantidad";
+    return false;
+  }
+
+  return true;
 }
+
+BUTTON_BORRAR.addEventListener("click", (e) => {
+  e.preventDefault();
+  INPUT_CANTIDAD.value = "";
+  RESULTADO.innerHTML = "Total a Pagar: $";
+  INPUT_CATEGORIA.value = "";
+  INPUT_NOMBRE.value = "";
+  INPUT_APELLIDO.value = "";
+  INPUT_CORREO.value = "";
+});
